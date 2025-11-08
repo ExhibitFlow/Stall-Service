97ea42183a1bc50ef128ff6848e578d00caba8da
PasanAbeysekara <pasankavindaabey@gmail.com>
2025-11-08 12:44:14 +0530

    Add initial application and test configuration files

---
+++ src/main/java/com/exhibitflow/stall/StallServiceApplication.java
@@ -0,0 +1,12 @@
+package com.exhibitflow.stall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StallServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StallServiceApplication.class, args);
+    }
+}
